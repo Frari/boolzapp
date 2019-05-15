d46a6e0a98f5dffa68f49e404dbbe85f42d4f914
Frari <francescoriccardi86@gmail.com>
2019-05-15 17:17:47 +0200

    problema selezione contatti

--- main.js
+++ main.js
@@ -1,14 +1,59 @@
 $('.invia').click(function(){
-// dichiaro variabile per prendere il valore dell'input immesso dall'utente
-  var text_utente = $('.input').val();
-// creo una variabile nella quale memorizzo il clone del contenitore con classe template in display none
-  var new_message = $('.mes_container.template').clone();
-// rimuovo dal contenitore del messaggio la classe template
-  new_message.removeClass('template');
-// aggiungo al figlio del contenitore la classe che da lo sfondo verde e inserisco il testo digitato dall'utente
-  new_message.children('.message_inv').addClass('me').text(text_utente);
-// inserisco tutto nel contenitore messaggi vuoto
-  $('.messaggi').append(new_message);
-// azzero l'input in modo che l'utente possa inserire altro messaggio
-  $('.input').val(' ');
+  send_message();
 });
+
+// funzione che fa inviare il messaggio anche schiacciando tasto invio
+  $('.input').keypress(function(event){
+    if(event.which == 13){
+      send_message();
+    }
+  });
+
+
+  function send_message(){
+  // dichiaro variabile per prendere il valore dell'input immesso dall'utente
+    var text_utente = $('.input').val();
+
+    if(text_utente){
+  // creo una variabile nella quale memorizzo il clone del contenitore con classe template in display none
+    var new_message = $('.mes_container.template').clone();
+  // rimuovo dal contenitore del messaggio la classe template
+    new_message.removeClass('template');
+  // aggiungo al figlio del contenitore la classe che da lo sfondo verde e inserisco il testo digitato dall'utente
+    new_message.children('.message_inv').addClass('me').text(text_utente);
+  // inserisco tutto nel contenitore messaggi vuoto
+    $('.messaggi').append(new_message);
+  // azzero l'input in modo che l'utente possa inserire altro messaggio
+    $('.input').val(' ');
+    }
+
+    setTimeout(function() {
+      // creo una variabile nella quale memorizzo il clone del contenitore con classe template in display none
+        var new_message = $('.mes_container.template').clone();
+        // rimuovo dal contenitore del messaggio la classe template
+        new_message.removeClass('template');
+        // aggiungo al figlio del contenitore la classe che da lo sfondo verde e inserisco il testo digitato dall'utente
+        new_message.children('.message_inv').addClass('computer').text();
+        // inserisco tutto nel contenitore messaggi vuoto
+        $('.messaggi').append(new_message);
+        // azzero l'input in modo che l'utente possa inserire altro messaggio
+        $('.input').val(' ');
+      }, 1000);
+
+
+  }
+
+  // creo funzione per cercare contatto al click della lente d'ingrandimento
+  $('.lente').click(function(){
+    var ricerca_cont = $('.search').val();
+    $('.search').val(' ');
+
+    $('.num_cont.nome_cont p').each(function(){
+      if($(this).text() == ricerca_cont){
+        alert('uguale');
+      }else{
+        alert('diverso');
+      }
+    })
+
+  });
